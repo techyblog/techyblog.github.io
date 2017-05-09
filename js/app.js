9bba4f8e59f30716b6cdb1c67f1c17672c79858b
techyblog <techblogservices@gmail.com>
2017-05-09 13:05:25 +0530

    added config files

---
+++ js/app.js
@@ -0,0 +1,53 @@
+angular.module("TechBlog", ["ngRoute", "textAngular", "firebase"])
+	.run(["$rootScope", function($rootScope){
+		  // Initialize Firebase
+			var config = {
+				apiKey: "AIzaSyBeNnqTYLjAaMfbldZqjmMXOdKWrG1CKek",
+				authDomain: "techblog-4bb01.firebaseapp.com",
+				databaseURL: "https://techblog-4bb01.firebaseio.com",
+				storageBucket: "techblog-4bb01.appspot.com",
+				messagingSenderId: "435978073211"
+			};
+			firebase.initializeApp(config);
+
+			$rootScope.$on("$locationChangeStart", function(event, next, current) { 
+				$rootScope.menuValue = next.split('/')[1];
+    });
+	}])    
+	.config(["$routeProvider",
+		function($routeProvider) {
+		$routeProvider.
+		  when("/login", {
+			templateUrl: 'template/Login/login.html',
+			controller: "LoginController",
+			controllerAs: 'loginctrl'
+		  }).
+		  when("/addblog", {
+			templateUrl: 'template/Add-blog/add-blog.html',
+			controller: "AddBlogController",
+			controllerAs: "addblogctrl"
+		  }).
+		  when("/allblog", {
+			templateUrl: 'template/All-blogs/allblogs.html',
+			controller: "AllBlogController",
+			controllerAs: "allblogctrl"
+		  }).
+		  when("/blogdetails/:blogId", {
+			templateUrl: 'template/Blog-details/BlogDetails.html',
+			controller: "BlogDetsController",
+			controllerAs: "blogDetsCtrl"
+		  }).
+		  when("/myprofile", {
+			templateUrl: 'template/My-profile/MyProfile.html',
+			controller: "MyProfileController",
+			controllerAs: "profileCtrl"
+		  }).
+		  when("/review", {
+			templateUrl: 'template/Reviewer/Reviewer.html',
+			controller: "ReviewerController",
+			controllerAs: "reviewCtrl"
+		  }).
+		  otherwise({
+			redirectTo: "/login"
+		  });
+	}]);
